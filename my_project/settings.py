19ffb7e9112cc466be37444261cda5e74fde31ec
Sophie Mcgee <sophiebmcgee@outlook.com>
2024-08-18 19:33:53 +0000

    Update settings to retrieve SECRET_KEY from environment variable

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ MY_TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2+g7s76a2gs*k(71tb&slht8c#_c-e*q)@!j2+0=el*+ek(j)z'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
