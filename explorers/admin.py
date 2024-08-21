8722154a9410d2e99b5c198daa0da1da18062e46
Sophie Mcgee <sophiebmcgee@outlook.com>
2024-08-21 15:24:25 +0000

    Add 'approve comments' admin action

--- explorers/admin.py
+++ explorers/admin.py
@@ -14,7 +14,13 @@ class EventAdmin(admin.ModelAdmin):
 # Register the Comment model
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'event', 'created_on', 'content')
+    list_display = ['user', 'event', 'created_on', 'approved']
     search_fields = ('content', 'user__username', 'event__title')
     list_filter = ('created_on', 'user')
     ordering = ('-created_on',)
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+    approve_comments.short_description = "Mark selected comments as approved"
