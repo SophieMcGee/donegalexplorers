9abbf4049abd8c10249e812eba2cba3ef77c5d66
Sophie Mcgee <sophiebmcgee@outlook.com>
2024-08-21 14:34:38 +0000

    Register event and comment models in admin.py file

--- explorers/admin.py
+++ explorers/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Event, Comment
 
-# Register your models here.
+# Register the Event model
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'location', 'date', 'time', 'created_on')
+    search_fields = ('title', 'description', 'location')
+    list_filter = ('date', 'author')
+    ordering = ('-date',)
+
+# Register the Comment model
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'created_on', 'content')
+    search_fields = ('content', 'user__username', 'event__title')
+    list_filter = ('created_on', 'user')
+    ordering = ('-created_on',)
