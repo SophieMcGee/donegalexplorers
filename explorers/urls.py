9dca4e4b02a7f70888f861546982e0ac60d768d7
Sophie Mcgee <sophiebmcgee@outlook.com>
2024-08-25 06:29:58 +0000

    Add admin.site.urls to urls.py file

--- explorers/urls.py
+++ explorers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Enables the admin interface and login page
     path('', views.my_blog, name='my_blog'),
     path('summernote/', include('django_summernote.urls')),
 ]
