f2a8b64e3d20e1f038e0cc8e5ed1289d36a052c6
Sophie Mcgee <sophiebmcgee@outlook.com>
2024-08-25 06:38:33 +0000

    Fix duplicate admin URL inclusion in explorers app

--- explorers/urls.py
+++ explorers/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Enables the admin interface and login page
     path('', views.my_blog, name='my_blog'),
     path('summernote/', include('django_summernote.urls')),
 ]
