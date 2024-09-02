81a0bc003f44c65385d6c84e329294c58aa7b1bc
Sophie Mcgee <sophiebmcgee@outlook.com>
2024-09-02 14:51:39 +0000

    Update WSGI and ASGI configurations to import env

--- my_project/asgi.py
+++ my_project/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+import env
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 
 application = get_asgi_application()
