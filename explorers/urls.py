3e015eba711ebb869307e2315745e84a4cce19c3
Sophie Mcgee <sophiebmcgee@outlook.com>
2024-09-02 13:53:03 +0000

    Fix name error for eventupdateview

--- explorers/urls.py
+++ explorers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import EventCreateView
+from .views import EventCreateView, EventUpdateView
 
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
